8981940a7418f85a12015a280d20923dcf897440
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2018-03-08 15:21:44 +0100

    add addPost funcionality with alerts

--- src/app.js
+++ src/app.js
@@ -5,9 +5,33 @@ import { ui } from './ui';
 
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listen for add posts
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
 function getPosts() {
     http
         .get('http://localhost:3000/posts')
         .then(data => ui.showPosts(data))
+        .catch(err => console.log(err));
+}
+
+function submitPost() {
+    const title = document.querySelector('#title').value;
+    const body = document.querySelector('#body').value;
+
+    const data = {
+        title,
+        body
+    };
+
+    // Create post
+
+    http
+        .post('http://localhost:3000/posts', data)
+        .then(data =>{
+            ui.showAlert('Post Added!', 'alert alert-success');
+            ui.clearFields();
+            getPosts();
+        })
         .catch(err => console.log(err));
 }
--- src/ui.js
+++ src/ui.js
@@ -15,13 +15,40 @@ class UI {
                     <div class="card-body">
                         <h4 class="card-title">${post.title}</h4>
                         <p class="card-text">${post.body}</p>
-                        <a href="#" class="edit card-link" data-id=${post-id}><i class="fa fa-pencil"></i></a>
-                        <a href="#" class="delete card-link" data-id=${post-id}><i class="fa fa-remove"></i></a>
+                        <a href="#" class="edit card-link" data-id=${post -
+                            id}><i class="fa fa-pencil"></i></a>
+                        <a href="#" class="delete card-link" data-id=${post -
+                            id}><i class="fa fa-remove"></i></a>
                     </div>
                 </div>
             `;
         });
         this.post.innerHTML = output;
+    }
+    showAlert(message, className) {
+        this.clearAlert();
+        const div = document.createElement('div');
+        div.className = className;
+        div.appendChild(document.createTextNode(message));
+        // get parent
+        const container = document.querySelector('.postsContainer');
+        const posts = document.querySelector('#posts');
+        // insert alert
+        container.insertBefore(div, posts);
+        // Delete after 3 secs
+        // setTimeout(() => {
+        //     this.clearAlert()
+        // }, 3000);
+    }
+    clearAlert() {
+        const currentAlert = document.querySelector('.alert');
+        if (currentAlert){
+            currentAlert.remove();
+        }
+    }
+    clearFields() {
+        this.titleInput.value = '';
+        this.bodyInput.value = '';
     }
 }
 
